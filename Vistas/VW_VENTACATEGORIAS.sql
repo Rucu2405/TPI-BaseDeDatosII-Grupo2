e0265b99101acd4aff011113fae6716206d551af
Agustin Leonel Zuloaga <agustinleonelzuloaga@gmail.com>
2025-06-16 18:32:08 -0300

    Se modifica vista por el cambio de nombre de la columna de tabla Categorias.

--- Vistas/VW_VENTACATEGORIAS.sql
+++ Vistas/VW_VENTACATEGORIAS.sql
@@ -1,11 +1,11 @@
 --Vista para obtener las ventas totales de cada categoría de buzos.
 CREATE VIEW VW_VENTACATEGORIAS AS 
-Select C.Nombre AS Categoria, 
+Select C.DescripcionCategoria AS Categoria, 
 SUM(DP.Cantidad) AS TotalVendidos
 From DetallePedido DP
 INNER JOIN Productos P ON DP.IDProducto = P.IDProducto
 INNER JOIN Categorias C ON P.IDCategoria = C.IDCategoria
-GROUP BY C.Nombre;
+GROUP BY C.DescripcionCategoria;
 
 --Hacemos SELECT de esa vista ordenando de mayor venta a menor venta.
 SELECT * FROM VW_VENTACATEGORIAS
