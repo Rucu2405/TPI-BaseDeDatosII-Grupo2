af904275ab08a52fda6153d91848035cdf69f68a
Rolo <84113089+rololevy@users.noreply.github.com>
2025-06-22 16:22:47 -0300

    Update tr_EliminarCliente.sql eliminar en cascada

--- Triggers/tr_EliminarCliente.sql
+++ Triggers/tr_EliminarCliente.sql
@@ -1,25 +1,33 @@
-CREATE OR ALTER TRIGGER tr_EliminarCliente
-ON Clientes
-INSTEAD OF DELETE
-AS
-BEGIN
-    -- Verificamos si tiene pedidos "en curso"
-    IF EXISTS (
-        SELECT 1
-        FROM deleted d
-        JOIN Pedidos p ON p.IDCliente = d.IDCliente
-        WHERE p.IDEstado IN (1, 2)
-    )
+    CREATE OR ALTER TRIGGER tr_EliminarCliente
+    ON Clientes
+    INSTEAD OF DELETE
+    AS
     BEGIN
-        RAISERROR('No se puede eliminar el cliente porque tiene pedidos en estado pendiente o enviado.', 16, 1);
-        RETURN;
-    END
+        -- Verifico si tiene pedidos "en curso"
+        IF EXISTS (
+            SELECT 1
+            FROM deleted d
+            JOIN Pedidos p ON p.IDCliente = d.IDCliente
+            WHERE p.IDEstado IN (1, 2)
+        )
+        BEGIN
+            RAISERROR('No se puede eliminar el cliente porque tiene pedidos en estado pendiente o enviado.', 16, 1);
+            RETURN;
+        END
 
-    -- Si no hay pedidos en curso, eliminamos primero los pedidos relacionados
-    DELETE FROM Pedidos
-    WHERE IDCliente IN (SELECT IDCliente FROM deleted);
+        -- Elimino primero los detalles de los pedidos del cliente
+        DELETE FROM DetallePedido
+        WHERE IDPedido IN (
+            SELECT IDPedido
+            FROM Pedidos
+            WHERE IDCliente IN (SELECT IDCliente FROM deleted)
+        );
 
-    -- Luego eliminamos al cliente
-    DELETE FROM Clientes
-    WHERE IDCliente IN (SELECT IDCliente FROM deleted);
-END;
+        -- Si no hay pedidos en curso, eliminamos primero los pedidos relacionados
+        DELETE FROM Pedidos
+        WHERE IDCliente IN (SELECT IDCliente FROM deleted);
+
+        -- Luego eliminamos al cliente
+        DELETE FROM Clientes
+        WHERE IDCliente IN (SELECT IDCliente FROM deleted);
+    END;
