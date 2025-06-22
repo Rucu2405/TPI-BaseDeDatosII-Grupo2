07d89b00db73f5bcf96b64888bad001f6474bf04
Julian Revidatti <julianrevidatti817@gmail.com>
2025-06-22 13:43:28 -0300

    Agregando mas info a la tabla ventas por talle, nuevo nombre ESTADISTICASPORTALLE

--- INSERT de datos para usar de prueba.sql
+++ INSERT de datos para usar de prueba.sql
@@ -76,6 +76,9 @@ GO
 INSERT INTO DetallePedido (IDPedido, IDProducto, Cantidad) VALUES
 (1, 1, 1),
 (2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
+(2, 2, 1),
 (3, 3, 1),
 (4, 4, 1),
 (5, 5, 2),
--- Vistas/VW_VENTASPORTALLA.sql
+++ Vistas/VW_VENTASPORTALLA.sql
@@ -1,14 +1,24 @@
 --Vista para obtener las ventas totales de cada talle de meyor a menor--
 
-CREATE VIEW VW_VENTASPORTALLA AS
+CREATE OR ALTER VIEW VW_ESTADISTICAS_VENTAS_POR_TALLE AS
 SELECT 
-    T.DescripcionTalle,
-    SUM(DP.Cantidad) AS TotalVendidos
-FROM DetallePedido DP
-JOIN Productos P ON DP.IDProducto = P.IDProducto
-JOIN Talles T ON P.IDTalle = T.IDTalle
-GROUP BY T.DescripcionTalle;
-GO
+    T.DescripcionTalle AS Talle,
+    COUNT(DISTINCT P.IDPedido) AS CantidadPedidos,
+    SUM(DP.Cantidad) AS UnidadesVendidas,
+    SUM(PR.Precio * DP.Cantidad) AS VentasBrutas,
+    SUM((PR.Precio * DP.Cantidad) * ISNULL(V.Descuento, 0) / 100) AS TotalDescuentos,
+    SUM((PR.Precio * DP.Cantidad) - ((PR.Precio * DP.Cantidad) * ISNULL(V.Descuento, 0) / 100)) AS VentasNetas,
+    ROUND(AVG(PR.Precio), 2) AS PrecioPromedioPorUnidad
+FROM 
+    Pedidos P
+    INNER JOIN DetallePedido DP ON P.IDPedido = DP.IDPedido
+    INNER JOIN Productos PR ON DP.IDProducto = PR.IDProducto
+    INNER JOIN Talles T ON PR.IDTalle = T.IDTalle
+    LEFT JOIN Vouchers V ON P.IDVoucher = V.IDVoucher
+GROUP BY 
+    T.DescripcionTalle
 
---Hacemos SELECT de esa vista ordenando de mayor venta a menor venta.
-SELECT * FROM VW_VENTASPORTALLA
+
+    --Hacemos SELECT de esa vista ordenando de mayor venta a menor venta.
+SELECT * FROM VW_ESTADISTICAS_VENTAS_POR_TALLE ORDER BY 
+    UnidadesVendidas DESC;
